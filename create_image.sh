2ef0a29d2a39fd50c8f5d3c00e2bda6873299182
louka <louka.menard.blondin@pm.me>
2025-01-24 00:05:26 -0500

    TODO: runit

--- create_image.sh
+++ create_image.sh
@@ -115,7 +115,7 @@ SYSROOT="$WORKDIR/sysroot"
 
 # Component versions
 KERNEL_VERSION="6.6.72"
-SYSTEMD_VERSION="257.2"
+RUNIT_VERSION="2.1.2"
 DRACUT_VERSION="059"
 BASH_VERSION="5.2.21"
 LUA_VERSION="5.4.7"
@@ -127,14 +127,12 @@ GNU_COREUTILS_VERSION="9.4"
 # Create directory structure
 mkdir -p "$WORKDIR" "$OUTPUT_DIR" "$SOURCES_DIR" "$SYSROOT"
 mkdir -p "$SYSROOT"/{bin,sbin,lib,lib64,usr,etc,var,boot,proc,sys,dev,run,tmp}
-
+mkdir -p "$SYSROOT/etc/runit/sv"
+mkdir -p "$SYSROOT/service"
 download_sources() {
-    # Create cache directory if it doesn't exist
     mkdir -p "$SOURCES_CACHE"
-
     cd "$SOURCES_CACHE"
 
-    # Function to download only if not in cache
     download_if_missing() {
         local url="$1"
         local filename=$(basename "$url")
@@ -149,10 +147,10 @@ download_sources() {
     # Download kernel
     download_if_missing "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KERNEL_VERSION.tar.xz"
 
-    # Download systemd
-    download_if_missing "https://github.com/systemd/systemd/archive/v$SYSTEMD_VERSION.tar.gz"
+    # Download runit
+    download_if_missing "http://smarden.org/runit/runit-$RUNIT_VERSION.tar.gz"
 
-    # Download initramfs bullshit
+    # Download initramfs
     download_if_missing "https://github.com/dracutdevs/dracut/archive/refs/tags/${DRACUT_VERSION}.tar.gz"
 
     # Download GNU components
@@ -165,7 +163,6 @@ download_sources() {
     # Download Lua
     download_if_missing "https://www.lua.org/ftp/lua-$LUA_VERSION.tar.gz"
 
-    # Copy cached files to sources directory
     mkdir -p "$SOURCES_DIR"
     cp -u *.tar.* "$SOURCES_DIR/"
 }
@@ -189,70 +186,113 @@ extract_sources() {
 
 build_kernel() {
     cd "$SOURCES_DIR/linux-$KERNEL_VERSION"
-
-    # Copy the provided .config
     cp "$WORKDIR/../.config" .
-
-    # Build kernel
     make -s V=0 -j$(nproc) 2>&1 | grep -E "error|Error|ERROR|warning|Warning|WARNING"
-
-    # Install kernel and modules
     make INSTALL_MOD_PATH="$SYSROOT" modules_install
     cp arch/x86_64/boot/bzImage "$SYSROOT/boot/vmlinuz"
 }
 
-build_systemd() {
-    cd "$SOURCES_DIR/systemd-$SYSTEMD_VERSION"
+build_runit() {
+    cd "$SOURCES_DIR/admin/runit-$RUNIT_VERSION"
 
-    mkdir -p build && cd build
-    meson setup \
-        --prefix=/usr \
-        --sysconfdir=/etc \
-        --localstatedir=/var \
-        -Drootprefix="/usr" \
-        -Dsplit-usr=false \
-        -Defi=true \
-        -Dbootloader=enabled \
-        -Dsysvinit-path=/etc/init.d \
-        -Dpamlibdir=/usr/lib/security \
-        -Drootlibdir=/usr/lib \
-        ..
+    # Compile runit
+    ./package/compile
 
-    ninja
-    DESTDIR="$SYSROOT" ninja install
-
-    # Create necessary symlinks
+    # Install runit
     mkdir -p "$SYSROOT/sbin"
-    ln -sf ../usr/lib/systemd/systemd "$SYSROOT/sbin/init"
+    cp command/* "$SYSROOT/sbin/"
+
+    # Create basic runit directories and scripts
+    mkdir -p "$SYSROOT/etc/runit"/{1,2,3}.d
+    mkdir -p "$SYSROOT/etc/sv"
+
+    # Create stage 1 script
+    cat >"$SYSROOT/etc/runit/1" <<'EOF'
+#!/bin/sh
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+# Mount virtual filesystems
+mountpoint -q /proc    || mount -t proc proc /proc -o nosuid,noexec,nodev
+mountpoint -q /sys     || mount -t sysfs sys /sys -o nosuid,noexec,nodev
+mountpoint -q /dev     || mount -t devtmpfs dev /dev -o mode=0755,nosuid
+mountpoint -q /dev/pts || mount -t devpts devpts /dev/pts -o mode=0620,gid=5,nosuid,noexec
+
+# Start udev
+if [ -x /sbin/udevd ]; then
+    /sbin/udevd --daemon
+    udevadm trigger --action=add --type=subsystems
+    udevadm trigger --action=add --type=devices
+    udevadm settle
+fi
+
+touch /etc/runit/stopit
+chmod 0 /etc/runit/stopit
+EOF
+
+    # Create stage 2 script
+    cat >"$SYSROOT/etc/runit/2" <<'EOF'
+#!/bin/sh
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+runsvdir -P /service 'log: ...........................................................................................................................................'
+EOF
+
+    # Create stage 3 script
+    cat >"$SYSROOT/etc/runit/3" <<'EOF'
+#!/bin/sh
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+echo 'Waiting for services to stop...'
+sv -w196 force-stop /service/*
+sv exit /service/*
+
+echo 'Stopping runsvdir...'
+killall runsvdir
+
+echo 'Unmounting filesystems...'
+umount -a -r
+
+# Power off
+echo 'Powering off...'
+halt -p
+EOF
+
+    chmod +x "$SYSROOT/etc/runit/"{1,2,3}
+
+    # Create basic services
+    mkdir -p "$SYSROOT/etc/sv/getty-tty1/"{log,supervise}
+    cat >"$SYSROOT/etc/sv/getty-tty1/run" <<'EOF'
+#!/bin/sh
+exec setsid getty 38400 tty1
+EOF
+    chmod +x "$SYSROOT/etc/sv/getty-tty1/run"
+
+    # Create symlink for default services
+    ln -s /etc/sv/getty-tty1 "$SYSROOT/service/"
 }
 
 build_bootloader() {
-    # Install systemd-boot
-    mkdir -p "$SYSROOT/boot/efi/EFI/BOOT"
-    cp "$SYSROOT/usr/lib/systemd/boot/efi/systemd-bootx64.efi" \
-        "$SYSROOT/boot/efi/EFI/BOOT/BOOTX64.EFI"
+    mkdir -p "$SYSROOT/boot/grub"
 
-    # Create basic loader configuration
-    mkdir -p "$SYSROOT/boot/loader/entries"
-    cat >"$SYSROOT/boot/loader/loader.conf" <<EOF
-default arch
-timeout 3
-editor 0
-EOF
+    cat >"$SYSROOT/boot/grub/grub.cfg" <<EOF
+set timeout=5
+set default=0
 
-    cat >"$SYSROOT/boot/loader/entries/arch.conf" <<EOF
-title Linux
-linux /vmlinuz
-initrd /initramfs.img
-options root=/dev/ram0 rw
+menuentry "Seed Linux" {
+    linux /boot/vmlinuz init=/sbin/runit-init root=/dev/ram0 rw
+    initrd /boot/initramfs.img
+}
 EOF
 }
 
 check_critical_files() {
     local files=(
         "$SYSROOT/bin/sh"
-        "$SYSROOT/usr/lib/systemd/systemd"
-        "$SYSROOT/usr/lib/systemd/system"
+        "$SYSROOT/sbin/runit"
+        "$SYSROOT/sbin/runit-init"
+        "$SYSROOT/etc/runit/1"
+        "$SYSROOT/etc/runit/2"
+        "$SYSROOT/etc/runit/3"
     )
 
     for file in "${files[@]}"; do
@@ -262,62 +302,45 @@ check_critical_files() {
         fi
     done
 }
-
 build_initramfs() {
     echo "Building and installing dracut..."
 
-    # Clean up any existing systemd service directories to prevent symlink conflicts
-    rm -rf "$SYSROOT/usr/lib/systemd/system/sysinit.target.wants"
-    rm -rf "$SYSROOT/usr/lib/systemd/system/initrd.target.wants"
-
-    # Extract and build dracut
     cd "$SOURCES_DIR"
     tar xf "${DRACUT_VERSION}.tar.gz"
     cd "dracut-${DRACUT_VERSION}"
 
-    # Configure and build
     ./configure \
         --prefix=/usr \
         --sysconfdir=/etc \
-        --libdir=/usr/lib \
-        --systemdsystemunitdir=/usr/lib/systemd/system
+        --libdir=/usr/lib
 
     make -j$(nproc)
     make DESTDIR="$SYSROOT" install
 
-    # Create necessary directories
     mkdir -p "$SYSROOT"/{etc/dracut.conf.d,var/tmp,run,sys,proc,dev}
     chmod 1777 "$SYSROOT/var/tmp"
 
-    # Configure dracut
     cat >"$SYSROOT/etc/dracut.conf.d/01-basic.conf" <<EOF
 # Add base drivers and filesystems
 add_drivers+=" ext4 "
-add_dracutmodules+=" base systemd kernel-modules fs-lib "
+add_dracutmodules+=" base kernel-modules fs-lib "
 force_drivers+=" brd "
 
 # Include basic system utilities
-install_items+=" /bin/sh /bin/bash /bin/mount /bin/umount /usr/lib/systemd/systemd "
-
-# Configure systemd as init
-systemd_enable="yes"
-systemd_path="/usr/lib/systemd/systemd"
+install_items+=" /bin/sh /bin/bash /bin/mount /bin/umount /sbin/runit /sbin/runit-init "
 EOF
 
-    # Check for critical files before running dracut
     check_critical_files
 
-    # Generate initramfs
     "$SYSROOT/usr/bin/dracut" \
         --force \
         --no-compress \
         --kver "$KERNEL_VERSION" \
-        --modules "base systemd kernel-modules fs-lib" \
+        --modules "base kernel-modules fs-lib" \
         --drivers "ext4 brd" \
         --sysroot "$SYSROOT" \
         --no-hostonly \
         --add-drivers "ext4 brd" \
-        --include "$SYSROOT/usr/lib/systemd/systemd" "$SYSROOT/usr/lib/systemd/systemd" \
         "$SYSROOT/boot/initramfs.img"
 
     if [ ! -f "$SYSROOT/boot/initramfs.img" ]; then
@@ -385,22 +408,19 @@ build_lua() {
 }
 
 create_iso() {
-    # Create ISO directory structure
     mkdir -p "$WORKDIR/iso/boot/grub"
     cp -r "$SYSROOT"/* "$WORKDIR/iso/"
 
-    # Generate GRUB configuration
     cat >"$WORKDIR/iso/boot/grub/grub.cfg" <<EOF
 set timeout=5
 set default=0
 
 menuentry "Seed Linux" {
-    linux /boot/vmlinuz init=/sbin/init root=/dev/ram0 rw initrd=/boot/initramfs.img
+    linux /boot/vmlinuz init=/sbin/runit-init root=/dev/ram0 rw
     initrd /boot/initramfs.img
 }
 EOF
 
-    # Create ISO
     grub-mkrescue -o "$OUTPUT_DIR/seedlinux.iso" "$WORKDIR/iso"
 }
 
@@ -421,7 +441,7 @@ main() {
     extract_sources
     build_gnu_userland
     build_bash
-    build_systemd
+    build_runit
     build_kernel
     build_bootloader
     build_initramfs
